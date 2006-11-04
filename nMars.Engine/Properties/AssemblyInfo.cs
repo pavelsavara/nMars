00bb967299ee37be4aa3a065d1c66f298262ced7
pavelsavara <pavelsavara@9a645708-d01a-0410-814a-dd0cde42ec34>
2006-11-04 14:00:41 +0000

    version info

--- nMars.Engine/Properties/AssemblyInfo.cs
+++ nMars.Engine/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly : AssemblyTitle("SimpleEngine")]
-[assembly : AssemblyDescription("")]
-[assembly : AssemblyConfiguration("")]
-[assembly : AssemblyCompany("")]
-[assembly : AssemblyProduct("Core")]
-[assembly : AssemblyCopyright("Copyright ©  2006")]
-[assembly : AssemblyTrademark("")]
-[assembly : AssemblyCulture("")]
+[assembly: AssemblyTitle("nMars RedCode engine")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -19,15 +9,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly : Guid("aea944b0-fe4c-4091-9610-4f8f1fb79e60")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly : AssemblyVersion("1.0.0.0")]
-[assembly : AssemblyFileVersion("1.0.0.0")]
---
+++ nMars.RedCode/Properties/VersionInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection; 
+[assembly: AssemblyCompany("Pavel Savara")]  
+[assembly: AssemblyProduct("nMars - Corewars MARS for .NET ")]  
+[assembly : AssemblyCopyright("Copyright ©  2006")]  
+[assembly : AssemblyVersion("0.4.0.0")]  
+[assembly : AssemblyFileVersion("0.4.0.0")]  
+[assembly: AssemblyDescription("http://sourceforge.net/projects/nmars")]  
