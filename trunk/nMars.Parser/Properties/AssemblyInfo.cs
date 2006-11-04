837463315baa57a7fdd8426e30846835f538b06d
pavelsavara <pavelsavara@9a645708-d01a-0410-814a-dd0cde42ec34>
2006-11-04 14:00:41 +0000

    version info

--- trunk/nMars.Parser/Properties/AssemblyInfo.cs
+++ trunk/nMars.Parser/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly : AssemblyTitle("Parser")]
-[assembly : AssemblyDescription("")]
-[assembly : AssemblyConfiguration("")]
-[assembly : AssemblyCompany("")]
-[assembly : AssemblyProduct("Parser")]
-[assembly : AssemblyCopyright("Copyright ©  2006")]
-[assembly : AssemblyTrademark("")]
-[assembly : AssemblyCulture("")]
+[assembly: AssemblyTitle("nMars RedCode parser")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -19,15 +9,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly : Guid("cdc8b2e9-6f31-4a14-aaae-8fbcda98bbd6")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly : AssemblyVersion("1.0.0.0")]
-[assembly : AssemblyFileVersion("1.0.0.0")]
--- trunk/pMars.ShellWrapper/Properties/AssemblyInfo.cs
+++ trunk/pMars.ShellWrapper/Properties/AssemblyInfo.cs
@@ -1,16 +1,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly : AssemblyTitle("pmParser")]
-[assembly : AssemblyDescription("")]
-[assembly : AssemblyConfiguration("")]
-[assembly : AssemblyCompany("")]
-[assembly : AssemblyProduct("pmParser")]
-[assembly : AssemblyCopyright("Copyright ©  2006")]
-[assembly : AssemblyTrademark("")]
-[assembly : AssemblyCulture("")]
+
+[assembly: AssemblyTitle("nMars wrapper around pMarsv.exe")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -19,15 +10,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly : Guid("e7e382d8-0105-4a23-b77d-dbaf39aa2443")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly : AssemblyVersion("1.0.0.0")]
-[assembly : AssemblyFileVersion("1.0.0.0")]
