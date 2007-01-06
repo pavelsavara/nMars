9dcf12e0fccbd2c049772e86f1af31d071be750e
pavelsavara <pavelsavara@9a645708-d01a-0410-814a-dd0cde42ec34>
2007-01-06 20:59:21 +0000

    version

--- trunk/nMars.RedCode/Properties/VersionInfo.cs
+++ trunk/nMars.RedCode/Properties/VersionInfo.cs
@@ -1,7 +1,6 @@
 using System.Reflection; 
 [assembly: AssemblyCompany("Pavel Savara")] 
 [assembly: AssemblyProduct("nMars - Corewars MARS for .NET ")] 
-[assembly: AssemblyVersion("0.8.2.2")] 
-[assembly: AssemblyFileVersion("0.8.2.2")] 
-[assembly: AssemblyCopyright("http://sourceforge.net/projects/nmars")] 
+[assembly: AssemblyVersion("0.8.2.0")] 
+[assembly: AssemblyFileVersion("0.8.2.0")] 
 [assembly: AssemblyDescription("http://sourceforge.net/projects/nmars")]
