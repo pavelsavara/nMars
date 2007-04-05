605c228fd778775d2abe03b0eb32b320cc7cb803
pavelsavara <pavelsavara@9a645708-d01a-0410-814a-dd0cde42ec34>
2007-04-05 20:17:35 +0000

    Core War renamed

--- nMars.RedCode/Properties/VersionInfo.cs
+++ nMars.RedCode/Properties/VersionInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection; 
 [assembly: AssemblyCompany("nMars")] 
-[assembly: AssemblyProduct("nMars - Corewars MARS for .NET ")] 
+[assembly: AssemblyProduct("nMars - Core War MARS for .NET ")] 
 [assembly: AssemblyVersion("0.9.1.0")] 
 [assembly: AssemblyFileVersion("0.9.1.0")] 
 [assembly: AssemblyCopyright("Pavel Savara - http://sourceforge.net/projects/nmars")] 
