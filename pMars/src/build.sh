78748e1f9f7cfa03cb79e6753834c024327dcc2b
Pavel Savara <pavel.savara@gmail.com>
2019-04-16 18:59:16 +0200

    compile pmars

--- pMars/src/build.sh
+++ pMars/src/build.sh
@@ -4,25 +4,20 @@ make all
 cp MakefileConsole Makefile
 make clean
 make all
-cp MakefileSDL Makefile
-make clean
-make all
 
 
 echo !!Package
 VER=9.3
-OS=FreeBSD
-#OS=Linux.i386
+#OS=FreeBSD
+OS=Linux.i386
 PKG=0.$VER-a.$OS
 
 rm -rf pMarsBinary.$PKG
 mkdir pMarsBinary.$PKG
 cp pmars-c pMarsBinary.$PKG
 cp pmars-cs pMarsBinary.$PKG
-cp pmars-sdl pMarsBinary.$PKG
 cd pMarsBinary.$PKG
 ln -s pmars-c pmars
 cd ..
 rm pMars.$PKG.tar.gz
 tar cvfz pMars.$PKG.tar.gz pMarsBinary.$PKG
-~/upload $OS $VER pMars.$PKG.tar.gz
